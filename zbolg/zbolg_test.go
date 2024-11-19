bc1bc3424534eb8cf9593f149bbe0428201f55fd
bbbaa <bbbaa6338@gmail.com>
2024-11-19 17:12:34 +0800

    添加主题的文件

--- zbolg/zbolg_test.go
+++ zbolg/zbolg_test.go
@@ -6,6 +6,11 @@ import (
 
 func zBolgInstallTest() {
 
+
+	NewZBolg(ZBolg{
+		WebSite:    "baidu.com",
+	})
+
 	//sites := []string{"glzsbz.com", "wkstny.com", "zml1976.com", "xiongxincailiao.com", "zgznsq.com", "hgmrjtss.com", "skgdsb.com", "knwsfwx.com", "liqingf.com", "cqyfxx.com", "lyjlnk.com", "zzqjdc.com", "ylsqgj.com", "huazhongchaxun.com", "yfby888.com", "mayibanjia365.com", "tjsjgsbxg.com", "shy5188.com", "fullerence.com", "firedreamphoto.com", "shjd-edu.com", "slpaishuiban.com", "ynhrpzs.com", "zsb018.com", "zshongx.com", "shcdcc.com", "lcmygg.com", "hdsjxsb.com", "1cy37.com", "wsroujiamo.com", "jcks888.com", "wlguolv0038.com", "yachhf.com", "tjchangronggg.com", "bccsoy.com"}
 	//
 	//for _, site := range sites {
